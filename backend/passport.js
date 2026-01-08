dfb893f8ba6b0313d3fbf44aba106ec868a12768
Chey <nwabugift0@gmail.com>
2026-01-08 13:39:45 +0000

    Implement Google OAuth strategy with Passport

---
+++ backend/passport.js
@@ -0,0 +1,54 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const User = require('../models/User');
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: `${process.env.BACKEND_URL}/api/auth/google/callback`
+  },
+  async (accessToken, refreshToken, profile, done) => {
+    try {
+      let user = await User.findOne({ googleId: profile.id });
+      
+      if (user) {
+        return done(null, user);
+      }
+      
+      user = await User.findOne({ email: profile.emails[0].value });
+      
+      if (user) {
+        user.googleId = profile.id;
+        user.picture = profile.photos[0]?.value;
+        await user.save();
+        return done(null, user);
+      }
+      
+      user = await User.create({
+        googleId: profile.id,
+        email: profile.emails[0].value,
+        name: profile.displayName,
+        picture: profile.photos[0]?.value
+      });
+      
+      return done(null, user);
+    } catch (error) {
+      return done(error, null);
+    }
+  }
+));
+
+passport.serializeUser((user, done) => {
+  done(null, user._id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (error) {
+    done(error, null);
+  }
+});
+
+module.exports = passport;
